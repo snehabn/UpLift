9b465e8c8dbb369ffcef26589fedd3eb951c81cc
Shin <mrmaxin@gmail.com>
2016-06-27 13:18:58 -0700

    add test user controller and view

--- app/scripts/controllers/users.js
+++ app/scripts/controllers/users.js
@@ -1,15 +1,15 @@
 'use strict';
 
 angular.module('upliftApp')
-  .controller('OrganizationsCtrl', function ($scope, $http) {
+  .controller('UsersCtrl', function ($scope, $http) {
 
     $http({
       method: 'GET',
-      url: 'https://blooming-cove-33951.herokuapp.com/organizations'
+      url: 'https://blooming-cove-33951.herokuapp.com/users'
     })
-    .then(function successCallback(response) {
-      console.log(response);
-      $scope.organization = response.data[0];
+    .then(function successCallback(response1) {
+      console.log(response1);
+      $scope.user = response1.data;
       // $scope.orgs_arr = response.organizations;
       // $scope.first_org = $scope.orgs_arr[0];
       // $scope.events = response.events;
