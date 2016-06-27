e61e2bf3d8fc2d53f611420d8baa29dcd18c3e34
Sneha Narasimhan <sbnarasimhan@gmail.com>
2016-06-27 13:30:14 -0700

    Add users service, controller, and view.

---
+++ app/scripts/controllers/users.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('upliftApp')
+  .controller('UsersCtrl',['$scope', 'UsersService', 
+
+    function ($scope, UsersService) {
+
+    // var controller = this;
+    // $http({
+    //   method: 'GET',
+    //   url: 'https://blooming-cove-33951.herokuapp.com/organizations'
+    // })
+    // .then(function successCallback(response) {
+    //   $scope.organization = response.data[0];
+    //   // $scope.orgs_arr = response.organizations;
+    //   // $scope.first_org = $scope.orgs_arr[0];
+    //   // $scope.events = response.events;
+    //   // $scope.eventsForOrg = $scope.getEventsByOrg($scope.events, 1);
+    // }, function errorCallback(response) {
+
+    // });
+    UsersService.getAll().then(function(data){
+      $scope.user = data.data;
+    });
+}]);
