50ab0f81e7074273b2b58edefeaedb9e0677039e
Sneha Narasimhan <sbnarasimhan@gmail.com>
2016-06-27 12:14:13 -0700

    Recreate controller to reflect service instead of http request.

--- app/scripts/controllers/organizations.js
+++ app/scripts/controllers/organizations.js
@@ -1,18 +1,26 @@
+'use strict';
+
 angular.module('upliftApp')
-  .controller('OrganizationsCtrl', function ($scope, $http) {
+  .controller('OrganizationsCtrl',['$scope', 'OrganizationsService', 
+
+    function ($scope, OrganizationsService) {
 
     // var controller = this;
-    $http({
-      method: 'GET',
-      url: 'https://blooming-cove-33951.herokuapp.com/organizations'
-    })
-    .then(function successCallback(response) {
-      $scope.organization = response.data[0];
-      // $scope.orgs_arr = response.organizations;
-      // $scope.first_org = $scope.orgs_arr[0];
-      // $scope.events = response.events;
-      // $scope.eventsForOrg = $scope.getEventsByOrg($scope.events, 1);
-    }, function errorCallback(response) {
+    // $http({
+    //   method: 'GET',
+    //   url: 'https://blooming-cove-33951.herokuapp.com/organizations'
+    // })
+    // .then(function successCallback(response) {
+    //   $scope.organization = response.data[0];
+    //   // $scope.orgs_arr = response.organizations;
+    //   // $scope.first_org = $scope.orgs_arr[0];
+    //   // $scope.events = response.events;
+    //   // $scope.eventsForOrg = $scope.getEventsByOrg($scope.events, 1);
+    // }, function errorCallback(response) {
 
+    // });
+    OrganizationsService.getAll().then(function(data){
+      console.log(data)
+      $scope.organization = data.data[0];
     });
-});
+}]);
