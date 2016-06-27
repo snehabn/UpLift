33d80faa7d40149abf830c2868a7e1baa688359c
Shin <mrmaxin@gmail.com>
2016-06-27 15:04:18 -0700

    add functional user page

--- app/scripts/controllers/users.js
+++ app/scripts/controllers/users.js
@@ -20,6 +20,15 @@ angular.module('upliftApp')
 
     // });
     UsersService.getAll().then(function(data){
-      $scope.users = data.data;
+      var tempUsers = data.data;
+      var tempUser1 = tempUsers[0]; //display 1 user for now
+
+      $scope.user = {
+        firstName: tempUser1.first_name,
+        lastName: tempUser1.last_name,
+        email: tempUser1.email,
+        zip: tempUser1.zip,
+        image: tempUser1.image_url
+      }
     });
 }]);
