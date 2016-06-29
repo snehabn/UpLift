b05094b5a981991043731144dd88770946f1b18e
Shin <mrmaxin@gmail.com>
2016-06-28 17:04:06 -0700

    fix minor errors of changing {} to [] in service.js

--- app/scripts/services/specificEvent.service.js
+++ app/scripts/services/specificEvent.service.js
@@ -3,7 +3,7 @@
 angular.module('upliftApp')
 .service('specificEventService', function($http){
 	
-	var specificEvent = {};
+	var specificEvent = [];
 	this.get = function(id) {
 		return $http.get('https://blooming-cove-33951.herokuapp.com/events/'+id+'/').success(function(data){
 			angular.copy(data, specificEvent);
