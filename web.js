7ca1b3fde64176cbf5445e3da7eb3f1d81a0828b
UmarFBajwa <umarbajwa786@gmail.com>
2016-06-29 13:50:17 -0700

    Add web.js and package.json

--- web.js
+++ web.js
@@ -1,8 +1,20 @@
-var gzippo = require('gzippo');
-var express = require('express');
-var morgan = require('morgan');
-var app = express();
+var path = require('path')
+var express = require('express')
+var port = (process.env.PORT || 8080)
 
-app.use(morgan('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+var server = {
+  app: function () {
+    var app = express()
+    var indexPath = path.join(__dirname, './dist/index.html')
+    var publicPath = express.static(path.join(__dirname, './dist'))
+
+    app.use('/dist', publicPath)
+    app.get('/', function (_, res) { res.sendFile(indexPath) })
+
+    return app
+  }
+}
+
+app = server.app()
+app.listen(port)
+console.log(`Listening at http://localhost:${port}`)
